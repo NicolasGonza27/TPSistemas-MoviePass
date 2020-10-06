c4463d9787d955620ee17feb6812f5fe204d3979
Nahuelmdp <62819293+Nahuelmdp@users.noreply.github.com>
2020-10-06 19:47:04 -0300

    Comit_1_(6/10/2020)

---
+++ Config/Config.php
@@ -0,0 +1,14 @@
+<?php namespace Config;
+
+define("ROOT", dirname(__DIR__) . "/");
+//Path to your project's root folder
+define("FRONT_ROOT", "/cursoPHP/Material/Practicos/Practico6/ExerciseTwo/");
+define("VIEWS_PATH", "Views/");
+define("CSS_PATH", FRONT_ROOT.VIEWS_PATH . "layout/styles/");
+define("JS_PATH", FRONT_ROOT.VIEWS_PATH . "js/");
+define("IMG_PATH", VIEWS_PATH . "img/");
+?>
+
+
+
+
