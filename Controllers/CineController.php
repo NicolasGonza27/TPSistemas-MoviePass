0ab7a71443abadbe431be176183b96e9601414d8
zelayalucass <luck.zelaya@gmail.com>
2020-10-08 19:25:55 -0300

    08-10-2020

--- Controllers/CineController.php
+++ Controllers/CineController.php
@@ -22,9 +22,9 @@
      
         
 
-        public function Add($id, $nombre, $direccion, $apertura, $cierre, $valor_entrada)
+        public function Add($id, $nombre, $direccion, $capacidad, $apertura, $cierre, $valor_entrada)
         {
-            $cine = new Cine($id, $nombre, $direccion, $apertura, $cierre, $valor_entrada);
+            $cine = new Cine($id, $nombre, $direccion, $capacidad, $apertura, $cierre, $valor_entrada);
 
             $this->cineDAO->Add($cine);
 
