f85aaec09ada8b848189d1fa90b220ece6265441
Nahuelmdp <nahuelmdp@hotmail.com>
2020-10-14 21:48:24 -0300

    Primera_entrega(14/10/2020)

--- Views/Views-Cliente/list-movie.php
+++ Views/Views-Cliente/list-movie.php
@@ -2,14 +2,40 @@
     
 <div class="container content espaciado-sup">
     <div class="row">
-        <?php foreach($listMovie as $movie) { ?>
-            <div class="col-2">
-                <form action="<?php echo FRONT_ROOT.'Movie/ShowContentViews';?>" method="post">
-                    <input class="hide" name="id" type="text" value="<?php echo $movie->getId(); ?>"></input>
-                    <input type="image" class="img-movies" src="<?php echo $movie->getImage();?>" alt="" width="80%" height="80%" title="<?php echo $movie->getTitle();?>">
-                </form>
-            </div>
-        <?php } ?>
+        
+        <?php 
+
+        if(!empty($listMovie))
+        {
+            foreach($listMovie as $movie) { ?>
+                <div class="col-2">
+                    <form action="<?php echo FRONT_ROOT.'Movie/ShowContentViews';?>" method="post">
+                        <?php if( $movie->getPoster_path()) {?>
+                        
+                            <input class="hide" name="id" type="text" value="<?php echo $movie->getId(); ?>"></input>
+                            <input type="image" class="img-movies" src="<?php echo $movie->getImage();?>" alt="" width="80%" height="80%" title="<?php echo $movie->getTitle();?>">
+                        
+                        <?php } else { ?>
+                            
+                            <input class="hide" name="id" type="text" value="<?php echo $movie->getId(); ?>"></input>
+                            <input type="image" class="img-movies" src="\dashboard\TPSistemas-MoviePass\Views\img\sin-Imagen-disponible.jpg" alt="" width="80%" height="80%" title="<?php echo $movie->getTitle();?>">
+                            
+                        <?php }?>
+                    
+                    </form>
+                </div>
+            <?php } 
+        
+        }
+        else
+        {
+            
+            echo "<script> if(confirm('No hay peliculas con  los requisitos especificados'));";
+            echo "window.location = '/dashboard/TPSistemas-MoviePass/Home/ShowFiltersViews';
+                </script>";
+               
+        }
+        ?>
     </div>
 </div>
    
